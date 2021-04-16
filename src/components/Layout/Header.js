ea75d6278af8fcda1e5495a986db1067584057f1
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-16 17:12:34 +0700

    init project

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div></div>
+    </>
+  );
+};
+
+export default Header;
